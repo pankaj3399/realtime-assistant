b9bf6d8228286e538eb43aeb6deaabd5dca9ec91
DHONDGE YASHVARDHAN CHATURBHUJ <yashvardhandhondgepc@outlook.com>
2025-01-19 22:10:57 +0530

    Update App.tsx

--- pdf_analyzer/Frontend/src/App.tsx
+++ pdf_analyzer/Frontend/src/App.tsx
@@ -1,14 +1,13 @@
 import {  useAuth } from '@clerk/clerk-react'
 import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import SignUpPage from './pages/SignUpPage';
-import Home from './pages/Home';
 import SignInPage from './pages/SignInPage';
 import LandingPage from './pages/LandingPage';
-import NewChat from './pages/NewChat';
-import Chat from './pages/Chat';
 import { useEffect } from 'react';
 import { setAuthToken } from './api/axiosClient';
+import Home from './pages/Home';
 import AudioChatPage from './pages/AudioChatPage';
+import GenerateReportPage from './pages/GenerateReport';
 
 
 const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
@@ -34,19 +33,14 @@ export default function App() {
         <Route path='/signin' element={<SignInPage />} />
         <Route path='/' element={<LandingPage />} />
         <Route path='/home' element={<Home />} />
-        <Route path='/chat/:id' element={
-          <ProtectedRoute>
-            <Chat />
-          </ProtectedRoute>
-        } />
-        <Route path='/newchat' element={
-          <ProtectedRoute>
-            <NewChat />
-          </ProtectedRoute>
-        } />
-        <Route path='/newaudiochat' element={
+        <Route path='/newaudiochat/:id' element={
           <ProtectedRoute>
             <AudioChatPage />
+          </ProtectedRoute>
+        } />
+        <Route path='/generatereport/:id' element={
+          <ProtectedRoute>
+            <GenerateReportPage />
           </ProtectedRoute>
         } />
       </Routes>
