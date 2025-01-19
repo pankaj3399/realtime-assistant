44395a0bdbec8c812697110aad150b05325dc777
DHONDGE YASHVARDHAN CHATURBHUJ <yashvardhandhondgepc@outlook.com>
2025-01-19 22:02:55 +0530

    Update index.js

--- pdf_analyzer/Backend/index.js
+++ pdf_analyzer/Backend/index.js
@@ -8,9 +8,11 @@ import dotenv from "dotenv"
 
 import {createServer} from "http"
 import { WebSocketServer } from 'ws';
-import { getClient, handleRealTimeMessages, processPdf } from './config/audioai.js';
+import { getClient, handleRealTimeMessages, processPdf, processPdfWithDB } from './config/audioai.js';
 import { extractPdfText } from './utils/extractPdfText.js';
 import { processWithAssistantText } from './config/assistant.js';
+import { createRouteHandler } from 'uploadthing/express';
+import { uploadRouter } from './config/uploadthing.js';
 dotenv.config()
 const app = express();
 const corsOptions = {
@@ -50,14 +52,10 @@ wss.on('connection', async (ws) => {
             },
           }
         });
+
+        
     
-        await aiClient.send({
-          type: "response.create",
-          response: {
-            modalities: ["audio","text"],
-            instructions: `Say "Hello" to start the conversation.`,
-          }
-        });
+        
         ws.on("message", async (message) => {
             try {
               const data = JSON.parse(message);
@@ -71,7 +69,9 @@ wss.on('connection', async (ws) => {
               
               switch (data.type) {
                 case "process_pdf": {
-                  await processPdf(aiClient, ws, data.pdfText);
+                  const prevConvo = data.analysis.transcripts.map(trs => `${trs.role}:${trs.text}`).join(" ");
+                  const pdfData = data.analysis.pdfData.join(" ")
+                  await processPdfWithDB(aiClient, ws, pdfData, prevConvo);
                   break;
                 }
                 case "session_update": {
@@ -89,10 +89,16 @@ wss.on('connection', async (ws) => {
                   break;
                 }
                 case "process_pdf_file": {
-                  const pdfTextChunks = await extractPdfText(data.fileData);          
-                  const pdfText = pdfTextChunks.join(" ");
-                  messages.push(`This is text for analysis: \n ${pdfText}`);
-                  await processPdf(aiClient, ws, pdfText);
+                  const prevConvo = data.analysis.transcripts.map(trs => `${trs.role}:${trs.text}`).join(" ");
+                  const pdfData = data.analysis.pdfData.join(" ")
+                  await processPdfWithDB(aiClient, ws, pdfData, prevConvo);
+                  await aiClient.send({
+                    type: "response.create",
+                    response: {
+                      modalities: ["audio", "text"],
+                      instructions: `Respond according to users question or instructions`,
+                    },
+                  });
                   break;
                 }
                 case "save_and_analyse":{
@@ -151,6 +157,13 @@ app.use(clerkMiddleware())
 app.use('/api/pdf', pdfAnalyzerRouter);
 app.use('/api/users',userRouter);
 
+app.use(
+  "/api/uploadthing",
+  createRouteHandler({
+    router: uploadRouter
+  }),
+);
+
 app.get("/", (req, res) => {
     res.send("Healthy Server")
 })
@@ -160,4 +173,3 @@ const PORT = process.env.PORT || 3002;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
