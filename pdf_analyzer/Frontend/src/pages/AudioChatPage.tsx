62c6f9ef442401e4b13c9a3a7842ada4aec60cfc
DHONDGE YASHVARDHAN CHATURBHUJ <yashvardhandhondgepc@outlook.com>
2025-01-19 22:08:59 +0530

    Update AudioChatPage.tsx

--- pdf_analyzer/Frontend/src/pages/AudioChatPage.tsx
+++ pdf_analyzer/Frontend/src/pages/AudioChatPage.tsx
@@ -1,11 +1,34 @@
-import AudioChat from '@/components/Audio/AudioChat'
+import apiClient from '@/api/axiosClient'
+import Conversation from '@/components/Audio/Conversation'
 import NavBar from '@/components/Home/navbar'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const AudioChatPage = () => {
+  const params = useParams()
+  const [analysis, setAnalysis] = useState<any>(null)
+
+  useEffect(()=>{
+    if(params.id){
+      getAnalysis()
+    }
+  },[params])
+
+  const getAnalysis = async () => {
+    try{
+      const res = await apiClient.get(`/api/users/analysis/${params.id}`);
+      if(res.data){
+          setAnalysis(res.data.analysis)
+      }
+    }catch(err){
+      console.log(err);
+    }
+  }
+
   return (
     <div className='p-2 min-h-screen flex flex-col font-inter'>
         <NavBar back />
-        <AudioChat />
+        {analysis && <Conversation analysis={analysis} />}
     </div>
   )
 }
